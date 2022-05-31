88ff693266cf89890aad5730a24a442802cb916b
Emmanuel Tolani <emmanuel@Emmanuels-MacBook-Air.local>
2022-05-31 14:58:35 -0600

    fixed obvious issue

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9fc0047cd247908fa0e1"; }
+/******/ 		__webpack_require__.h = function() { return "891346671bea032c7e98"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.3ae1160d93338d3fa67d.hot-update.js
+++ .next/static/webpack/webpack.3ae1160d93338d3fa67d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ef9b6dcadb92664174ba"; }
+/******/ 	__webpack_require__.h = function() { return "891346671bea032c7e98"; }
 /******/ }();
 /******/ 
 /******/ }
