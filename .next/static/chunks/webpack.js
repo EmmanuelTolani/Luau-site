14a5d9c655da6bb75784632292d963d8d420b259
Emmanuel Tolani <emmanuel@Emmanuels-MacBook-Air.local>
2022-06-01 11:29:14 -0600

    removed usless page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "891346671bea032c7e98"; }
+/******/ 		__webpack_require__.h = function() { return "d54443a325e16ad0b83a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
